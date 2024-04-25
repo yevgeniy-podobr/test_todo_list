f477947b6efb62337de973bb1391fd92353038c3
zhenia.podobreev <zhenia.podobreev@cruxlab.com>
2024-04-26 01:37:05 +0300

    added active class for tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import './app.scss';
-import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+import { Navigate, Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import * as route from './services/routes'
 import { AllToDos, CompletedToDos, Input } from './components';
 
 function App() {
   const navigation = useNavigate()
+  const location = useLocation()
+
   return (
     <div className="app">
       <header className='app__header container'>
         <Input />
         <div className="app__header-tabs">
-          <button className='app__header-tabs_tab button' onClick={() => navigation(`${route.all}`)}>All</button>
-          <button className='app__header-tabs_tab button' onClick={() => navigation(`${route.completed}`)}>Completed</button>
+          <button 
+            className={`app__header-tabs_tab button ${location.pathname === route.all && 'active'}`} 
+            onClick={() => navigation(`${route.all}`)}>All</button>
+          <button 
+            className={`app__header-tabs_tab button ${location.pathname === route.completed && 'active'}`} 
+            onClick={() => navigation(`${route.completed}`)}>Completed</button>
         </div>
       </header>
       <main className="app__main container">
